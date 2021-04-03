5b88bc6ff4a809ba6d1ed5a8eceee6d8112deb70
Somenath95 <connectsomenath@gmail.com>
2021-04-03 15:37:47 +0530

    Delete data and recreate blogs

--- src/main/java/com/jhipster/demo/config/CacheConfiguration.java
+++ src/main/java/com/jhipster/demo/config/CacheConfiguration.java
@@ -48,6 +48,11 @@ public class CacheConfiguration {
             createCache(cm, com.jhipster.demo.domain.User.class.getName());
             createCache(cm, com.jhipster.demo.domain.Authority.class.getName());
             createCache(cm, com.jhipster.demo.domain.User.class.getName() + ".authorities");
+            createCache(cm, com.jhipster.demo.domain.Blog.class.getName());
+            createCache(cm, com.jhipster.demo.domain.Entry.class.getName());
+            createCache(cm, com.jhipster.demo.domain.Entry.class.getName() + ".tags");
+            createCache(cm, com.jhipster.demo.domain.Tag.class.getName());
+            createCache(cm, com.jhipster.demo.domain.Tag.class.getName() + ".entries");
             // jhipster-needle-ehcache-add-entry
         };
     }
--- src/main/webapp/app/entities/entity-routing.module.ts
+++ src/main/webapp/app/entities/entity-routing.module.ts
@@ -4,6 +4,21 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   imports: [
     RouterModule.forChild([
+      {
+        path: 'blog',
+        data: { pageTitle: 'Blogs' },
+        loadChildren: () => import('./blog/blog.module').then(m => m.BlogModule),
+      },
+      {
+        path: 'entry',
+        data: { pageTitle: 'Entries' },
+        loadChildren: () => import('./entry/entry.module').then(m => m.EntryModule),
+      },
+      {
+        path: 'tag',
+        data: { pageTitle: 'Tags' },
+        loadChildren: () => import('./tag/tag.module').then(m => m.TagModule),
+      },
       /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
     ]),
   ],
